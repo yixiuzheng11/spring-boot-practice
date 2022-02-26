4cd2c1ba2309bd6f66a0310f98ef01cda1e72fa4
yixiuzheng <yixiuzheng11@163.com>
2022-02-26 20:34:12 +0800

    排除redis的自动配置

--- src/main/java/org/yixz/SpringBootPracticeApplication.java
+++ src/main/java/org/yixz/SpringBootPracticeApplication.java
@@ -2,8 +2,13 @@ package org.yixz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={
+        RedisAutoConfiguration.class,
+        RedisRepositoriesAutoConfiguration.class
+})
 public class SpringBootPracticeApplication {
 
     public static void main(String[] args) {
